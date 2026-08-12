f93462516ebd89d4f2d5b37cfc2c5c2971f93a90
zcharka <zcharkayt@gmail.com>
2026-08-12 06:53:22 +0200

    Update greeter.nix

--- DE/greeter.nix
+++ DE/greeter.nix
@@ -4,5 +4,6 @@ services = {
   displayManager.sddm.enable = true;
   displayManager.gdm.enable = false;
   displayManager.cosmic-greeter.enable = false;
+  xserver.displayManager.lightdm.enable = true;
 };
 }
