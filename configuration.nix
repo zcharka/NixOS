5b70d7eda1587b9c97aa195a7cef93f7c44b458c
zcharka <zcharkayt@gmail.com>
2026-08-16 11:49:41 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -6,12 +6,13 @@
       ./hardware-configuration.nix
       ./hardware.nix
       ./bootloader.nix
-      ./users.nix
       ./programs.nix
       ./services.nix
       ./audio.nix
       ./etc.nix
       ./network.nix
+# użytkownicy
+      ./users/sebastian.nix
 # środowiska graficzne
       ./DE/greeter.nix
       ./DE/plasma.nix
